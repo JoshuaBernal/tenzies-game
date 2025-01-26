2db51aad555572819debfc6f75e972cd50fba2b1
Joshua Miguel Bernal <joshua_bernal@outlook.com>
2025-01-26 18:18:22 +0800

    added new features

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import './App.css'
 import Die from './Components/Die'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
-
+import { useStopwatch } from 'react-timer-hook'
 
 function App() {
+  const {
+    seconds,
+    minutes,
+    hours,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: false })
 
   const [dieObject, setDieObject] = React.useState(() => generateNumbers())
+  const [rollCount, setRollCount] = React.useState(0)
+  const [attemptCount, setAttemptCount] = React.useState(1)
+  const [gameStarted, setGameStarted] = React.useState(false)
+  const [gameWon, setGameWon] = React.useState(false)
 
-  let gameWon = false
+  React.useEffect(() => {
+    const values = dieObject.map(num => num.value)
+    const allSame = new Set(values).size === 1
+    const allHeld = dieObject.every(die => die.isHeld)
 
-  const values = dieObject.map(num => num.value);
-  const allSame = new Set(values).size === 1;
-  const allHeld = dieObject.every(die => die.isHeld);
-    
-  if (allSame && allHeld) {
-      gameWon = true
-  }
+    if (allSame && allHeld) {
+      setGameWon(true)
+      pause()
+    } else {
+      setGameWon(false)
+    }
+  }, [dieObject, pause])
 
   function generateNumbers() {
     return new Array(10)
@@ -29,9 +44,9 @@ function App() {
         }))
   }
 
-
   function rollDice() {
     const newValue = generateNumbers()
+    setRollCount(prevCount => prevCount + 1)
 
     setDieObject(
       prevState => prevState.map(
@@ -41,8 +56,16 @@ function App() {
   }
 
   function newGame() {
-    gameWon = false
+    setGameWon(false)
     setDieObject(generateNumbers())
+    setRollCount(0)
+    setAttemptCount(prevCount => prevCount + 1)
+    reset()
+  }
+  
+  function startGame() {
+    setGameStarted(true)
+    start()
   }
 
   function hold(id) {
@@ -54,12 +77,12 @@ function App() {
   }
 
   const dieNumbers = dieObject.map(
-    dieObject => <Die key={dieObject.id} value={dieObject.value} isHeld={dieObject.isHeld} toggle={() => hold(dieObject.id)}/>
+    dieObject => <Die key={dieObject.id} value={dieObject.value} isHeld={dieObject.isHeld} toggle={() => hold(dieObject.id)} gameStarted={gameStarted}/>
   )
 
   // START OF CODE FOR RENDERING
   return (
-    <main className='h-full rounded-xl flex justify-center items-center flex-col'>
+    <main className='h-auto rounded-xl flex justify-center items-center flex-col py-8'>
 
       {gameWon && <Confetti width={window.innerWidth - 1} height={window.innerHeight - 1}/>}
 
@@ -67,20 +90,38 @@ function App() {
         {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
       </div>
 
-      <h1 className='text-3xl font-semibold'>Tenzies</h1>
+      <h1 className='text-3xl font-semibold mb-2'>Tenzies</h1>
 
       <p className='text-md text-center mx-12 mb-4 text-gray-700'>
         Roll until all dice are the same. Click each die to 
         freeze it at its current value between rolls.
       </p>
 
-      <div className="grid grid-cols-5 gap-x-2 gap-y-2">
+      <div className='font-semibold mb-3 text-indigo-700'>
+        <h3>Elapsed Time: {minutes > 60 && `${hours}:`}{seconds > 60 && `${minutes}:`}{seconds}</h3>
+      </div>
+
+      <div className="grid grid-cols-5 gap-x-2 gap-y-2 mb-4">
         {dieNumbers}
       </div>
 
-      <button onClick={gameWon ? newGame : rollDice} className='bg-buttonColor text-white py-2 px-10 rounded shadow-lg shadow-gray-400 font-semibold mt-10'>
+      {gameWon && <p className='text-green-600 font-bold text-lg'>
+        You won!
+      </p>}
+
+      {gameStarted && <button onClick={gameWon ? newGame : rollDice} className='bg-buttonColor text-white py-2 px-6 rounded shadow-lg shadow-gray-400 font-semibold mt-4 w-36 flex justify-center items-center gap-x-2'>
+        {!gameWon && <img src="./rolldice.png" className='w-6'/>}
         {gameWon ? "New Game" : "Roll"}
-      </button>
+      </button>}
+      {!gameStarted && <button onClick={startGame} className='bg-buttonColor text-white py-2 px-4 rounded shadow-lg shadow-gray-400 font-semibold mt-4 w-36 flex items-center justify-center gap-x-2'>
+        <img src="./startgame.png" className='w-4'/>
+        Start Game
+      </button>}
+
+      <div className='mt-6 flex-row text-center'>
+        <p className='font-semibold'>Rolls: {rollCount}</p>
+        <p className='font-semibold'>Attempts: {attemptCount}</p>
+      </div>
 
     </main>
   )
--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Die(props) {
     const conditionalStyle = {
-        backgroundColor: props.isHeld ? '#59E391' : 'white',
+        backgroundColor: props.isHeld ? '#59E391' : 'WHITE',
+        opacity: props.gameStarted ? '100%' : '50%'
     }
 
     return (
@@ -12,8 +13,9 @@ export default function Die(props) {
             aria-pressed={props.isHeld} 
             aria-label={`Die with value ${props.value}, ${props.isHeld ? "held": "not held"}`} 
             className='rounded-lg shadow-md w-11 p-2 m-2 font-extrabold text-xl'
+            disabled={!props.gameStarted}
         >
-            {props.value}
+            <img src={`die-${props.value}.png`} />
         </button>
     )
 }
